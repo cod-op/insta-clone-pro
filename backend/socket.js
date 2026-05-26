1a1beb8e249755b448c629ef872f833ffd2ac7c7
Shlok Kumar Patel <shlokpatel094@gmail.com>
2026-05-26 19:55:42 +0530

    Update socket.js

--- backend/socket.js
+++ backend/socket.js
@@ -6,7 +6,7 @@ const server = http.createServer(app)
 
 const io=new Server(server,{
     cors:{
-        origin:"https://insta-pro-backend.onrender.com",
+        origin:"https://insta-clone-pro-app.vercel.app",
         methods:["GET","POST"]
     }
 })
