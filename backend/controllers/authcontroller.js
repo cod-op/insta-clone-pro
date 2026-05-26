d74a20da16a6755fe901075e3a0276cd94cf4cd0
Shlok Kumar Patel <shlokpatel094@gmail.com>
2026-05-26 19:37:27 +0530

    Update authcontroller.js

--- backend/controllers/authcontroller.js
+++ backend/controllers/authcontroller.js
@@ -50,8 +50,8 @@ const signUp=async(req,res)=>{
 
    res.cookie("token",token,{
     httpOnly:true,
-    secure:false,
-    sameSite:"Strict",
+    secure:true,
+    sameSite:"none",
     maxAge:10*365*24*60*60*1000
    } )
 
